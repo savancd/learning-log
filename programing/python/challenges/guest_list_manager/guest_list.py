2129741b4312f7de365a96fe35ebb019870ed880
sava <m.sava84@gmail.com>
2026-06-30 23:25:06 +0000

    update: Python section

--- programing/python/challenges/guest_list_manager/guest_list.py
+++ programing/python/challenges/guest_list_manager/guest_list.py
@@ -7,7 +7,7 @@ with open("guest_list.json", "r", encoding="utf-8") as f:
 
 # checking data type
 print(type(data))
-
+#print(data)
 
 print("These are keys: ")
 for i, key in enumerate(data.keys()):
@@ -20,3 +20,20 @@ print(data["event"])
 print(f"This is  data:  {data['event']['name']}")
 
 print(data["event"]["date"].replace("_", "."))
+
+# printing the keys from a .json
+for key in data.keys():
+	print(f"These are the keys: {key}")
+
+# printing values from a dict
+for i, item in enumerate(data["guest_list"]):
+	print(f"This is the value {i}: {item}")
+
+	# printing all the key and values from a list
+	# to enumerate through both keys and values must enclose them inside ()
+	for i, (key, values) in enumerate(item.items()):
+		print(f"\t{i}: {key}: {values}")
+	print()
+
+
+#for i, n in data["guest_list"]:
